9bf2012c1f18c71aa25efbdb6aaab9bb9a682afb
Gabriel Arrillaga <gabriel@macrostax.com>
2020-07-06 22:27:57 -0600

    chore: fix warnings
    - add mix.lock to repo
    - add LSP files to .gitignore

--- mix.exs
+++ mix.exs
@@ -1,25 +1,26 @@
 defmodule PhoenixAlexa.Mixfile do
   use Mix.Project
 
-  @version File.read!("VERSION") |> String.strip
+  @version File.read!("VERSION") |> String.trim()
 
   def project do
     [app: :phoenix_alexa,
      version: @version,
      elixir: "~> 1.2",
      description: "Alexa library for Phoenix",
-     deps: deps,
-     package: package,
-     consolidate_protocols: Mix.env != :test]
+     deps: deps(),
+     package: package(),
+     consolidate_protocols: Mix.env() != :test]
   end
 
   def application do
-    [applications: []]
+    [applications: [:plug]]
   end
 
   defp deps do
-    [{:poison, "~> 2.0"},
-     {:plug, "~> 1.1"},
+    [
+      {:poison, "~> 2.0"},
+      {:plug, "~> 1.10"}
     ]
   end
 
